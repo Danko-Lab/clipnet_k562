777a95747cb705f3ff04e50a682813a019b27a5c
Adam Youlin He <ayh8@cornell.edu>
2024-05-15 03:52:55 -0400

    delete

--- data_processing_scripts/index_fasta.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/env bash
-
-# Adds record number to each header
-awk '{if (/^>/) print ">"(++i)"_" substr($0,2); else print $0;}' $1
-
-# Replaces header with record number
-#awk '/^>/{print ">" ++i; next}{print}' $1
