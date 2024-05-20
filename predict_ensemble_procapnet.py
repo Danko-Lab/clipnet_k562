d0cbe41b32f4b3a26e31214e04d8f4ec69b24c80
Adam Youlin He <ayh8@cornell.edu>
2024-05-20 09:10:17 -0400

    updated utils

--- predict_ensemble_procapnet.py
+++ predict_ensemble_procapnet.py
@@ -1,5 +1,12 @@
 import procapnet
 import numpy as np
 import h5py
+import glob
+import utils
 
-def predict_ensemble_procapnet(X, model_dir, batch_size=64, logits=False):
+paths = glob.glob("models/procapnet_k562/*.model")
+models = [procapnet.ProCapNet(p) for p in paths]
+
+ref_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_ref.fa.gz") / 2
+alt_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_alt.fa.gz") / 2
+
