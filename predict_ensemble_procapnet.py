7c8add0b20befbdc566740b0338f2a7184843661
Adam Youlin He <ayh8@cornell.edu>
2024-05-20 13:50:32 -0400

    added benchmark for procapnet

--- predict_ensemble_procapnet.py
+++ predict_ensemble_procapnet.py
@@ -4,6 +4,7 @@ import h5py
 import glob
 import utils
 import torch
+import tqdm
 
 paths = glob.glob("models/procapnet_k562/*.model")
 models = [procapnet.Model(p).cuda() for p in paths]
@@ -11,5 +12,24 @@ models = [procapnet.Model(p).cuda() for p in paths]
 ref_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_ref.fa.gz").swapaxes(1, 2) / 2
 alt_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_alt.fa.gz").swapaxes(1, 2) / 2
 
+ref_pred = []
+for model in tqdm.tqdm(models):
+    ref_pred.append(model.predict(torch.tensor(ref_seqs).to(torch.float).cuda()))
 
-models[0].predict(torch.tensor(ref_seqs[:100]).to(torch.float).cuda())
+alt_pred = []
+for model in tqdm.tqdm(models):
+    alt_pred.append(model.predict(torch.tensor(alt_seqs).to(torch.float).cuda()))
+
+ref_profile = np.mean([p[0] for p in ref_pred], axis=0)
+ref_quantity = np.mean([p[1] for p in ref_pred], axis=0)[:, 0]
+
+alt_profile = np.mean([p[0] for p in alt_pred], axis=0)
+alt_quantity = np.mean([p[1] for p in alt_pred], axis=0)[:, 0]
+
+with h5py.File("data/mpra/k562_mpra_snps_ref_procapnet.h5", "w") as f:
+    f.create_dataset("profile", data=ref_profile, compression="gzip")
+    f.create_dataset("quantity", data=ref_quantity, compression="gzip")
+
+with h5py.File("data/mpra/k562_mpra_snps_alt_procapnet.h5", "w") as f:
+    f.create_dataset("profile", data=alt_profile, compression="gzip")
+    f.create_dataset("quantity", data=alt_quantity, compression="gzip")
--- siraj_mpra/evaluate_prediction.py
+++ siraj_mpra/evaluate_prediction.py
@@ -2,6 +2,8 @@ import h5py
 import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 
 ref = h5py.File("/home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_ref.h5")["quantity"][
     :, 0
@@ -9,13 +11,12 @@ ref = h5py.File("/home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_ref.h5")["qua
 alt = h5py.File("/home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_alt.h5")["quantity"][
     :, 0
 ]
-random = h5py.File("/home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_shuffle1.h5")[
-    "quantity"
-][:, 0].mean()
+ref_p = h5py.File("/home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_ref_procapnet.h5")["quantity"]
+alt_p = h5py.File("/home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_alt_procapnet.h5")["quantity"]
 
 mpra = pd.read_csv("media-4-K562_allelic_mpra.tsv.gz", sep="\t")
 snps = pd.read_csv("media-3_oligos_snps.tsv.gz", sep="\t")
-pred = pd.DataFrame({"ref": ref, "alt": alt, "variant": snps["Variant"]})
+pred = pd.DataFrame({"ref": ref, "alt": alt, "ref_p": ref_p, "alt_p": alt_p, "variant": snps["Variant"]})
 
 data = pred.merge(mpra, left_on="variant", right_on="variant")
 data["expt"] = np.log2(
@@ -23,7 +24,12 @@ data["expt"] = np.log2(
     / (data["mean_RNA_alt_K562"] / data["mean_Plasmid_alt_K562"])
 )
 data["pred"] = np.log2(data["ref"] / data["alt"])
+data["pred_p"] = np.log2(data["ref_p"] / data["alt_p"])
 data.dropna(inplace=True)
 
-pearsonr(data["expt"], data["pred"])
+pearsonr(data["expt"], data["pred_p"])
+clf = LogisticRegression()
+clf.fit(data["pred"].values.reshape(-1, 1), data["emVar_K562"].values)
+roc_auc_score(data["emVar_K562"], clf.predict_proba(data["pred"].values.reshape(-1, 1))[:, 1])
+
 # PearsonRResult(statistic=0.24444150001598372, pvalue=0.0)
