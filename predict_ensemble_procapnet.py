507231415b27fbf934981982ff9973d3d8924a0a
Adam Youlin He <ayh8@cornell.edu>
2024-05-22 12:26:05 -0400

    added plotting of full length mpra

--- predict_ensemble_procapnet.py
+++ predict_ensemble_procapnet.py
@@ -1,27 +1,42 @@
-import procapnet
-import numpy as np
-import h5py
 import glob
-import utils
+import os
+
+import h5py
+import numpy as np
 import torch
 import tqdm
+
+import procapnet
+import utils
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 paths = glob.glob("models/procapnet_k562/*.model")
 models = [procapnet.Model(p).cuda() for p in paths]
 
-ref_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_2114_ref.fa.gz").swapaxes(1, 2) / 2
-alt_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_2114_alt.fa.gz").swapaxes(1, 2) / 2
-
+ref_seqs = (
+    utils.get_twohot_fasta_sequences(
+        "data/mpra/k562_mpra_snps_2114_ref.fa.gz"
+    ).swapaxes(1, 2)
+    / 2
+)
 ref_pred = []
 for model in tqdm.tqdm(models):
     ref_pred.append(model.predict(torch.tensor(ref_seqs).to(torch.float).cuda()))
 
+ref_profile = np.mean([p[0] for p in ref_pred], axis=0)
+ref_quantity = np.mean([p[1] for p in ref_pred], axis=0)[:, 0]
+
+
+alt_seqs = (
+    utils.get_twohot_fasta_sequences(
+        "data/mpra/k562_mpra_snps_2114_alt.fa.gz"
+    ).swapaxes(1, 2)
+    / 2
+)
 alt_pred = []
 for model in tqdm.tqdm(models):
     alt_pred.append(model.predict(torch.tensor(alt_seqs).to(torch.float).cuda()))
-
-ref_profile = np.mean([p[0] for p in ref_pred], axis=0)
-ref_quantity = np.mean([p[1] for p in ref_pred], axis=0)[:, 0]
 
 alt_profile = np.mean([p[0] for p in alt_pred], axis=0)
 alt_quantity = np.mean([p[1] for p in alt_pred], axis=0)[:, 0]
