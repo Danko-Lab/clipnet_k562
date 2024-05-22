3c77a85cba96faeb4942ca1abe0ee7edd21bc739
Adam Youlin He <ayh8@cornell.edu>
2024-05-22 02:46:03 -0400

    fixed typo

--- predict_ensemble_procapnet.py
+++ predict_ensemble_procapnet.py
@@ -9,8 +9,8 @@ import tqdm
 paths = glob.glob("models/procapnet_k562/*.model")
 models = [procapnet.Model(p).cuda() for p in paths]
 
-ref_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_ref.fa.gz").swapaxes(1, 2) / 2
-alt_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_alt.fa.gz").swapaxes(1, 2) / 2
+ref_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_2114_ref.fa.gz").swapaxes(1, 2) / 2
+alt_seqs = utils.get_twohot_fasta_sequences("data/mpra/k562_mpra_snps_2114_alt.fa.gz").swapaxes(1, 2) / 2
 
 ref_pred = []
 for model in tqdm.tqdm(models):
@@ -26,10 +26,10 @@ ref_quantity = np.mean([p[1] for p in ref_pred], axis=0)[:, 0]
 alt_profile = np.mean([p[0] for p in alt_pred], axis=0)
 alt_quantity = np.mean([p[1] for p in alt_pred], axis=0)[:, 0]
 
-with h5py.File("data/mpra/k562_mpra_snps_ref_procapnet.h5", "w") as f:
+with h5py.File("data/mpra/k562_mpra_snps_2114_ref_procapnet.h5", "w") as f:
     f.create_dataset("profile", data=ref_profile, compression="gzip")
     f.create_dataset("quantity", data=ref_quantity, compression="gzip")
 
-with h5py.File("data/mpra/k562_mpra_snps_alt_procapnet.h5", "w") as f:
+with h5py.File("data/mpra/k562_mpra_snps_2114_alt_procapnet.h5", "w") as f:
     f.create_dataset("profile", data=alt_profile, compression="gzip")
     f.create_dataset("quantity", data=alt_quantity, compression="gzip")
--- siraj_mpra/README.md
+++ siraj_mpra/README.md
@@ -31,16 +31,17 @@ python predict_ensemble.py \
     /home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_ref.h5 \
     --model_dir /home2/ayh8/clipnet_k562/models/clipnet_k562/ \
     --gpu --use_specific_gpu 1
-
 python predict_ensemble.py \
     /home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_alt.fa.gz \
     /home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_alt.h5 \
     --model_dir /home2/ayh8/clipnet_k562/models/clipnet_k562/ \
     --gpu --use_specific_gpu 0
-
 python predict_ensemble.py \
     /home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_shuffle1.fa.gz \
     /home2/ayh8/clipnet_k562/data/mpra/k562_mpra_snps_shuffle1.h5 \
     --model_dir /home2/ayh8/clipnet_k562/models/clipnet_k562/ \
     --gpu --use_specific_gpu 0
+
+cd ../
+
 ```
