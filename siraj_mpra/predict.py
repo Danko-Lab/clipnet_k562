7d069b549fef2f2d9a3d1db3672ae6f8b84ffb9a
Adam Youlin He <ayh8@cornell.edu>
2024-05-23 02:30:46 -0400

    Create predict.py

---
+++ siraj_mpra/predict.py
@@ -0,0 +1,33 @@
+import h5py
+import numpy as np
+import pandas as pd
+
+oligos = pd.read_csv("../../siraj_mpra/media-3_oligos_snps.tsv.gz", sep="\t")
+variants = pd.read_csv("../../siraj_mpra/media-3_oligos_snps.tsv.gz", sep="\t")[
+    "Variant"
+]
+
+mpra = pd.read_csv("../../siraj_mpra/media-4-K562_allelic_mpra.tsv.gz", sep="\t")
+mpra["expt"] = np.log2(
+    (mpra.mean_RNA_ref_K562 / mpra.mean_Plasmid_ref_K562)
+    / (mpra.mean_RNA_alt_K562 / mpra.mean_Plasmid_alt_K562)
+)
+
+oligos["pred_p_2114"] = np.log2(
+    h5py.File("k562_mpra_snps_2114_ref_procapnet.h5")["quantity"][:]
+    / h5py.File("k562_mpra_snps_2114_alt_procapnet.h5")["quantity"][:]
+)
+
+oligos["pred_p"] = np.log2(
+    h5py.File("k562_mpra_snps_ref_procapnet.h5")["quantity"][:]
+    / h5py.File("k562_mpra_snps_alt_procapnet.h5")["quantity"][:]
+)
+
+oligos["pred"] = np.log2(
+    h5py.File("k562_mpra_snps_ref.h5")["quantity"][:]
+    / h5py.File("k562_mpra_snps_alt.h5")["quantity"][:]
+)
+
+predictions = oligos.merge(mpra, left_on="Variant", right_on="variant")[
+    ["Variant", "expt", "pred", "pred_p", "pred_p_2114"]
+]
