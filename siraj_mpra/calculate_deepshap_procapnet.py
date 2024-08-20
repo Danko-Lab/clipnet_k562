f1627571d28c73aaa9ad6b6cf1000b326b7d0ccf
Adam Youlin He <ayh8@cornell.edu>
2024-08-20 12:15:00 -0400

    Update calculate_deepshap_procapnet.py

--- siraj_mpra/calculate_deepshap_procapnet.py
+++ siraj_mpra/calculate_deepshap_procapnet.py
@@ -18,7 +18,7 @@ class ProfileWrapper(torch.nn.Module):
 
     def forward(self, X):
         logits = self.model(X)[0]
-        return (self.softmax(logits) * logits).sum(dim=-1).sum(dim=-1)
+        return (self.softmax(logits) * logits).sum(dim=-1).sum(dim=-1).squeeze()
 
 
 class CountsWrapper(torch.nn.Module):
