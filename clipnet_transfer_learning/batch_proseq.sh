65454cd0eb32990eae894266255d9418c40f3324
Adam Youlin He <ayh8@cornell.edu>
2024-10-02 15:45:42 -0400

    Update batch_proseq.sh

--- clipnet_transfer_learning/batch_proseq.sh
+++ clipnet_transfer_learning/batch_proseq.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=k562_proseq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --time=72:00:00
 #SBATCH --partition=gpu
